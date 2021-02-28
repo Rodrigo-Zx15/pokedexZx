6a929a7c56a8ee3b0d59ea4b24d8517ba2d21a8a
Rodrigo N <rodrigo.gomes65@etec.sp.gov.br>
2021-02-28 20:54:55 -0300

    feat: added error handling and made the code more readable

--- dex.js
+++ dex.js
@@ -1,62 +1,67 @@
 //seletores
   //botoes+input  
-const prev = document.querySelector('#button-prev');
-const next = document.querySelector('#button-next');
-const input = document.querySelector('#poke-search');
-const btn = document.querySelector('#button-search');
+const buttonPrevious = document.querySelector('#button-prev');
+const buttonNext = document.querySelector('#button-next');
+const userInput = document.querySelector('#poke-search');
+const searchButton = document.querySelector('#button-search');
 
   //estaticos
 const img = document.querySelector('.img-thumbnail');
-const nameScreen = document.querySelector('#name-screen p');
-const type1 = document.querySelector('#type-1'); 
-const type2 = document.querySelector('#type-2'); 
-const stats = Array.from(document.querySelectorAll('#stats div:nth-child(n+7)'));
+const nameBox = document.querySelector('#name-screen p');
+const type1Box = document.querySelector('#type-1'); 
+const type2Box = document.querySelector('#type-2'); 
+const statsBoxes = Array.from(document.querySelectorAll('#stats div:nth-child(n+7)'));
 
 
 //sim.
 let id = 0; 
-let prevType = null;
-let prevType2 = null;
+let previousType;
+let previousType2;
 
 //functions
 function randomDex(){
     return Math.floor(Math.random() * (899-1) + 1);
 }
 async function getPoke(id){
-    const req = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-    const data = req.data;
-    return data;
+    try{
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+        const data = response.data;
+        return data;
+    }catch(error){
+        alert('Sorry! An error has ocurred: ' + error);
+    }
+        
 }
 
 function placeData(pokemon){
     //console.log(pokemon);
-    nameScreen.innerText = `#${pokemon.id}: ${pokemon.name.toUpperCase()}`;
+    nameBox.innerText = `#${pokemon.id}: ${pokemon.name.toUpperCase()}`;
     img.src = pokemon.sprites.other['official-artwork'].front_default;;
     
     
     //console.log(pokemon.stats);
     for(let i = 0;i<6;i++){
-        stats[i].innerText = pokemon.stats[i].base_stat;
+        statsBoxes[i].innerText = pokemon.stats[i].base_stat;
     }
     console.log(pokemon.types);
     if(pokemon.types.length > 1){
-        type2.style.display = '';
+        type2Box.style.display = '';
         currentType2 = pokemon.types[1].type.name;
-        type2.classList.toggle(`${prevType2}`);
-        type2.classList.toggle(`${currentType2}`);
-        type2.innerText = currentType2;
-        prevType2 = currentType2;
+        type2Box.classList.toggle(`${previousType2}`);
+        type2Box.classList.toggle(`${currentType2}`);
+        type2Box.innerText = currentType2;
+        previousType2 = currentType2;
         
     }else{
-        type2.style.display = 'none';
+        type2Box.style.display = 'none';
         //prevType2 = currentType2;
     }
 
     currentType = pokemon.types[0].type.name;
-    type1.classList.toggle(`${prevType}`);
-    type1.innerText = currentType;
-    type1.classList.toggle(`${currentType}`);
-    prevType = currentType;
+    type1Box.classList.toggle(`${previousType}`);
+    type1Box.innerText = currentType;
+    type1Box.classList.toggle(`${currentType}`);
+    previousType = currentType;
     
     id = pokemon.id;
 }
@@ -69,7 +74,7 @@ window.addEventListener('load', async function(){
     placeData(pokemon);
 
 })
-prev.addEventListener('click',async function(){
+buttonPrevious.addEventListener('click',async function(){
     id--;
     if(id < 1){
         alert('Invalid dex ID');
@@ -78,7 +83,7 @@ prev.addEventListener('click',async function(){
     let pokemon = await getPoke(id);
     placeData(pokemon);
 })
-next.addEventListener('click', async function(){
+buttonNext.addEventListener('click', async function(){
     id++;
     if(id > 898){
         alert('Invalid dex ID');
@@ -88,9 +93,9 @@ next.addEventListener('click', async function(){
     placeData(pokemon);
 })
 
-btn.addEventListener('click', async function(){
-    search = input.value;
-    input.value = '';
+searchButton.addEventListener('click', async function(){
+    let search = userInput.value;
+    userInput.value = '';
     let pokemon = await getPoke(search.toLowerCase());
     placeData(pokemon); 
 });
